b525691d3b83d94d1489fa68c28e6aa7dacd14be
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-02 21:01:51 +0200

    Add Exact Match Routes in the App.js

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import Header from './src/components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './src/components/Header';                                
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+    <Header />
+    <main className="container">
+      <Switch>
+        <Route exact path="/">
+          <Books />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </main>
+  </Router>
   );
 }
 
