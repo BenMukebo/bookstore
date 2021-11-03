9b492cc9d70f0ecebad21c1c903b1b5842532aa2
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-03 09:48:40 +0200

    Set initial state to be an empty array of books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -4,3 +4,27 @@ import { v4 as uuidv4 } from 'uuid';
 
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+
+const initialState = [
+  {
+    id: uuidv4(),
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    genre: 'Action',
+    completed: 64,
+  },
+  {
+    id: uuidv4(),
+    title: 'Dune',
+    author: 'Frank Herbert',
+    genre: 'Science Fiction',
+    completed: 8,
+  },
+  {
+    id: uuidv4(),
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    genre: 'Economy',
+    completed: 0,
+  },
+];
