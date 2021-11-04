1877597f3f5037b5e35e66a36d97ace450e5c91a
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-04 14:46:45 +0200

    Implement a createBook function wich take books's element

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,3 +2,20 @@
 const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/9oGCGsuz2srGR7ssHMEo';
 
 
+export const createBook = async ({ id, title, category }) => { //POST
+  const url = `${BASE_URL}/books`;
+  const data = JSON.stringify({
+    item_id: id,
+    title,
+    category,
+  });
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data,
+  });
+  return response.text();
+};
+
