09777b4ecc0dff3b587860ff1b851c73fd8252fb
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-04 15:23:47 +0200

    Remove initial state

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -10,29 +10,6 @@ import { v4 as uuidv4 } from 'uuid';
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [
-  {
-    id: uuidv4(),
-    title: 'The Hunger Games',
-    author: 'Suzanne Collins',
-    genre: 'Action',
-    completed: 64,
-  },
-  {
-    id: uuidv4(),
-    title: 'Dune',
-    author: 'Frank Herbert',
-    genre: 'Science Fiction',
-    completed: 8,
-  },
-  {
-    id: uuidv4(),
-    title: 'Capital in the Twenty-First Century',
-    author: 'Suzanne Collins',
-    genre: 'Economy',
-    completed: 0,
-  },
-];
 
 export const addBook = (payload) => ({
   type: ADD_BOOK,
@@ -44,16 +21,17 @@ export const removeBook = (payload) => ({
   payload,
 });
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [
         ...state,
         {
-          id: uuidv4(),
+          // id: uuidv4(),
+          id: action.payload.id,
           title: action.payload.title,
-          author: action.payload.author,
-          completed: 0,
+          // author: action.payload.author,
+          // completed: 0,
           genre: action.payload.genre,
         },
       ];
